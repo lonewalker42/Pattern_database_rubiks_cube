d46083dc0897b05ce5e2507c3715abfa909ab20c
tushar2405 <80542167+lonewalker42@users.noreply.github.com>
2021-07-27 14:00:57 +0530

    Create mymath.h

---
+++ mymath.h
@@ -0,0 +1,13 @@
+/*
+ * This file contains precomputed values for mathematical
+ * functions within the basic operation of the search algorithm.
+ */
+#ifndef MYMATH
+#define MYMATH
+
+extern const int two_to_the[10];
+extern const unsigned three_to_the[8];
+
+extern const unsigned fact[13];
+
+#endif
